03c3daa8430991b9157b312dbda75044ec8a5ff1
dhqhf <dhqhf@DESKTOP-S02JBER>
2023-01-01 22:56:30 +0900

    tiles 관련 라이브러리 추가 중 에러 발생, 해결 못 함

--- SpringWeb/src/main/java/com/ktw/web/controller/customer/NoticeController.java
+++ SpringWeb/src/main/java/com/ktw/web/controller/customer/NoticeController.java
@@ -13,7 +13,8 @@ public class NoticeController {
 		
 		model.addAttribute("test", "Hello~ Devtool"); // test에다가 Hello라고 끼우겠다
 		
-		return "customer/notice/list"; //홈부터 경로를 써준다
+//		return "customer/notice/list"; //홈부터 경로를 써준다, ResourceViewResolver에게 이 페이지를 찾아달라
+		return "customer.notice.list"; //홈부터 경로를 써준다, TilesViewResolver에게 이 페이지를 찾아달라
 	}
 	
 	@RequestMapping("detail") // list,detail,edit,reg,del
